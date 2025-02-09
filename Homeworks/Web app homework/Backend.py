418a08a6509692870fe6c64eaf847f1b9d62ef6e
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-09 13:28:30 +0100

    Prep work for streamlit integration.

--- Homeworks/Web app homework/Backend.py
+++ Homeworks/Web app homework/Backend.py
@@ -1,17 +1,29 @@
 import logging
-
 import numpy as np
 import math
+import streamlit as st
+from Functionalities import parser, bisection_protocols, newtons_method
 
 class Backend_deployment():
-    def __init__(self, a: [float, int], b: [float, int], tolerance: float, iterations: int, equation: str, deployment: int):
+    def __init__(self, a: [float, int], b: [float, int], tolerance: float, iterations: int, equation: str, deployment: int, testing_mode: int):
         self.a = a
         self.b = b
         self.tolerance = tolerance
         self.iterations = iterations
         self.equation = equation
         self.deployment_clearance = deployment
+        self.testing_mode = testing_mode
     def deploy(self):
         if self.deployment_clearance != 1:
             logging.info("Deployment bricked. Manually reactivate.")
             quit("Deployment bricked. Manually reactivate.")
+
+        extracted_function, derived_function = parser(self.equation)
+        # -> Bijection <-
+        bijection_point, bij_iterations_used, bij_errors = bisection_protocols(extracted_function, self.a, self.b, tolerance=self.tolerance)
+        # -> Newtons method <-
+        x, nm_iterations_used, nm_errors = newtons_method(f=extracted_function,df=derived_function,x_0=self.a, tolerance=self.tolerance, max_iterations=self.iterations)
+
+        if self.testing_mode != 1:
+            # - Streamlit deployment -
+            logging.info("Testing mode inactive")
