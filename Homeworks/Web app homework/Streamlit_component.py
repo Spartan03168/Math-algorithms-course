f6593e0caf161b44dfc8b48a44cc0f3054c1a102
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-09 13:05:33 +0100

    Fleshing out in progress.

--- Homeworks/Web app homework/Streamlit_component.py
+++ Homeworks/Web app homework/Streamlit_component.py
@@ -1,15 +1,14 @@
 import streamlit as st
-
+from Backend import Backend_deployment
 
 # - Behavior mods -
 developer_mode = 1
 web_deployment = 0
 
-st.title("Root finding web app assignment")
 if web_deployment == 1:
-    pass
-    # -> User inputs <-
-
-    # -> Deploy <-
-
-    # -> Convergence plot <-
+    st.title("Root finding web app assignment")
+    a = 0
+    b = 0
+    formula = "(5x+4)+1"
+    tolerance = 0.5
+    Backend_deployment(a=a, b=b, tolerance=tolerance, iterations=500, equation=formula, deployment=web_deployment)
