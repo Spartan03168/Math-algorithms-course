95122ed4d82068a1989b0d41383cc365d1624c1f
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-09 13:05:12 +0100

    Bricking protocols active.

--- Homeworks/Web app homework/Backend.py
+++ Homeworks/Web app homework/Backend.py
@@ -1,12 +1,17 @@
+import logging
+
 import numpy as np
 import math
 
 class Backend_deployment():
-    def __init__(self, a: [float, int], b: [float, int], tolerance: float, iterations: int, equation: str):
+    def __init__(self, a: [float, int], b: [float, int], tolerance: float, iterations: int, equation: str, deployment: int):
         self.a = a
         self.b = b
         self.tolerance = tolerance
         self.iterations = iterations
         self.equation = equation
+        self.deployment_clearance = deployment
     def deploy(self):
-        pass
+        if self.deployment_clearance != 1:
+            logging.info("Deployment bricked. Manually reactivate.")
+            quit("Deployment bricked. Manually reactivate.")
