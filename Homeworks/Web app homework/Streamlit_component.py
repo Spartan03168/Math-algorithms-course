1a6ceee73908c213ea434abf02f920fe55010a77
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-09 12:54:15 +0100

    Early streamlit shell.

---
+++ Homeworks/Web app homework/Streamlit_component.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+# - Behavior mods -
+developer_mode = 1
+web_deployment = 0
+
+st.title("Root finding web app assignment")
+if web_deployment == 1:
+    pass
+    # -> User inputs <-
+
+    # -> Deploy <-
+
+    # -> Convergence plot <-
