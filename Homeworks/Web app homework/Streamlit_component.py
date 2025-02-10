964bae9b1fea5e20ac1dd2f6a10e750595cb9d1e
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-10 10:02:02 +0100

    Testing mode expansion.

--- Homeworks/Web app homework/Streamlit_component.py
+++ Homeworks/Web app homework/Streamlit_component.py
@@ -11,11 +11,17 @@ web_deployment = 1
 if web_deployment == 1:
     if testing_mode != 1:
         st.title("Root finding web app assignment")
-    a = -4 if testing_mode == 1 else st.number_input("Interval start (a): ", value=-10.0)
-    b = 9 if testing_mode == 1 else st.number_input("Interval end (b): ", value=10.0)
-    iterations_cnt = 500 if testing_mode == 1 else st.number_input("Max iterations: ", value=100, step=10)
-    formula = "(5*x+4)+1" if testing_mode == 1 else st.text_input("Enter function (Example format, x**2 - 5*sp.sin(x) + x - 1): ")
-    tolerance = 0.5 if testing_mode == 1 else st.number_input("Tolerance: ", value=1e-6, format="%.1e")
+    a = int(input("a: ")) if testing_mode == 1 else st.number_input("Interval start (a): ", value=-10.0)
+    b = int(input("b: ")) if testing_mode == 1 else st.number_input("Interval end (b): ", value=10.0)
+    if testing_mode == 1:
+        if a > b:
+            print("a is bigger than be. This is compatible. a has to be smaller.")
+            while(a > b):
+                a = int(input("a: "))
+                b = int(input("b: "))
+    iterations_cnt = int(input("Iterations to test: ")) if testing_mode == 1 else st.number_input("Max iterations: ", value=100, step=10)
+    formula = input("Enter function (Example format, x**2 - 5*sin(x) + x - 1): ") if testing_mode == 1 else st.text_input("Enter function (Example format, x**2 - 5*sin(x) + x - 1): ", value="x**2 - 5*sin(x) + x - 1")
+    tolerance = float(input("Tolerance: ")) if testing_mode == 1 else st.number_input("Tolerance: ", value=1e-6, format="%.1e")
     # - Class deployment protocols -
     deployment_protocols = Backend_deployment(a=a, b=b, tolerance=tolerance, iterations=iterations_cnt, equation=formula, deployment=web_deployment, testing_mode=testing_mode)
     extracted_function, derived_function, bijection_point, bij_iterations_used, bij_errors, x, nm_iterations_used, nm_errors = deployment_protocols.deploy()
