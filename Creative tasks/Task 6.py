39ae25c26cb2ab3a3f1b6711f9fa1225786a75f2
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-14 07:51:27 +0100

    Successful first chart.

---
+++ Creative tasks/Task 6.py
@@ -0,0 +1,13 @@
+# Parametric function circle
+import numpy as np
+import matplotlib.pyplot as plt
+
+theta = np.linspace(0, 2*np.pi, 100)
+r = 16 * (np.sin(theta)**3)
+
+plt.figure(figsize=(7, 7))
+ax = plt.subplot(111, projection='polar')
+ax.plot(theta, r, color='blue', linewidth=2)
+
+ax.set_title(r'$16 \sin^3(\theta)$', fontsize=16, fontweight='bold')
+plt.show()
