a33e2a51df58ef7094eea4d6edd7161810f8346b
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-19 17:22:18 +0100

    Updating of linear regression.

--- Final project/Functionalities.py
+++ Final project/Functionalities.py
@@ -18,23 +18,30 @@ def linear_regression_projector(data_injection: [list, np.ndarray], source_DF, f
     model_applied = LinearRegression()
     if developer_mode == 1:
         print("Model initiated")
-    # - x train, x test, y train, y test splitting -
+        print(f"Model details:\n{model_applied}")
+    # - x train, x test, y train, y test splitting(Notes: Use all 3 features) -
     X = source_DF[[features]]
     y = source_DF[target]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     if developer_mode == 1:
+        print(f"X: {X}")
+        print(f"y: {y}")
         print("Data splitting completed")
     # - Deploy forecast -
     model_applied.fit(X_train, y_train)
     projection = model_applied.predict(X_test)
+    r_squared = model_applied.score(X_train, y_train)
+    coeff = model_applied.coef_
     if developer_mode == 1:
+        print(f"Coefficients: {coeff}")
+        print(f"R squared of forecast\n{r_squared}")
         print("Forecast completed")
     # - End processing time -
     end = datetime.datetime.now() - start
     # - Accuracy calculations -
     mse_tracked = mean_squared_error(y_test, projection)
     mae_tracked = mean_absolute_error(y_test, projection)
-    r2_tracked = r2_score(y_test, projection)
+    r2_tracked = r_squared
     if developer_mode == 1:
         print("Accuracy diagnostics calculated\n")
     # - Return statements -
