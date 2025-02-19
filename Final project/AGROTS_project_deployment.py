df7a54360df552580cd6202248500e0feb9ed755
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-19 08:00:36 +0100

    Prep work in progress.

--- Final project/AGROTS_project_deployment.py
+++ Final project/AGROTS_project_deployment.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import datetime
 
 # - Behaviour modifiers -
@@ -16,3 +13,30 @@ source_dataframe = pd.read_csv(fr"{file_path}")
 if developer_mode == 1:
     print("Dataframe info:")
     print(source_dataframe.info())
+    print()
+
+# - Prep work -
+time_stamps = source_dataframe["Timestamp"].to_numpy()
+air_pressure_data = source_dataframe["AirPressure"].to_numpy()
+air_temperature_A = source_dataframe["AirTemperatureA"].to_numpy()
+air_temperature_B = source_dataframe["AirTemperatureB"].to_numpy()
+air_humidity = source_dataframe["AirHumidity"].to_numpy()
+
+air_temp_A_processing_df = pd.DataFrame({
+    "Time stamps": time_stamps,
+    "Air pressure": air_pressure_data,
+    "Air temperature A": air_temperature_A
+    })
+
+air_temp_B_processing_df = pd.DataFrame({
+    "Time stamps": time_stamps,
+    "Air pressure": air_pressure_data,
+    "Air temperature B": air_temperature_B
+    })
+
+air_humidity_processing_df = pd.DataFrame({
+    "Time stamps": time_stamps,
+    "Air pressure": air_pressure_data,
+    "Air humidity": air_humidity
+    })
+
