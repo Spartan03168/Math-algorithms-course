ffc63334ee05b3a1b6ee910e5fa9f697d7a9efca
Tommaso Amorosi <tomy.bawse@gmail.com>
2025-02-20 13:45:26 +0100

    Integration of late stage refinements in progress.

--- Final project/AGROTS_project_deployment.py
+++ Final project/AGROTS_project_deployment.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime
+import datetime, os
 import seaborn as sns
 from Functionalities import linear_regression_projector, polynomial_regression_projector
 
+# - Autodownload folder path -
+dl_path = "C:\Users\Tomy\PycharmProjects\Math_algorithm_work\Final project\Saved_downloads\Recent"
+os.chdir(dl_path)
 # - Behaviour modifiers -
 developer_mode = 1
+deploy_switch = 0
 # -----------------------
 project_start = datetime.datetime.now()
 
@@ -19,7 +23,7 @@ source_df_corr = source_dataframe.corr()
 plt.figure(figsize=(10,8))
 sns.heatmap(source_df_corr,annot=True,cmap='coolwarm',fmt='.4f',linewidths=0.5)
 plt.title("Source data correlation matrix heat map")
-plt.show()
+plt.savefig("Source_data_heatmap.png")
 
 print(f"NANs detected: {source_dataframe.isnull().sum()}\n")
 if developer_mode == 1:
@@ -42,21 +46,21 @@ plt.scatter(air_temperature_A, air_pressure_data)
 plt.xlabel("Air temperature A")
 plt.ylabel("Air pressure")
 plt.title("Air pressure VS Air temperature A")
-plt.show()
+plt.savefig("Air_pressure_VS_air_temp_A.png")
 plt.close()
 # - Air temperature B VS air pressure
 plt.scatter(air_temperature_B, air_pressure_data)
 plt.xlabel("Air temperature B")
 plt.ylabel("Air pressure")
 plt.title("Air pressure VS Air temperature B")
-plt.show()
+plt.savefig("Air_pressure_VS_air_temp_B.png")
 plt.close()
 # - Air humidity VS air pressure
 plt.scatter(air_humidity, air_pressure_data)
 plt.xlabel("Air humidity")
 plt.ylabel("Air pressure")
 plt.title("Air pressure VS Air humidity")
-plt.show()
+plt.savefig("Air_pressure_VS_air_humidity.png")
 plt.close()
 # ---> Correleation coeficients <---
 process_data = pd.DataFrame({
@@ -71,56 +75,57 @@ print(corr_matrix)
 plt.figure(figsize=(10,8))
 sns.heatmap(corr_matrix,annot=True,cmap='coolwarm',fmt='.4f',linewidths=0.5)
 plt.title("Correlation matrix heat map")
-plt.show()
+plt.savefig("Processing_corr_matrix_heat_map.png")
 plt.close()
 # --------------------------
 # ----> Deployment <----
-# - Air temperature A -
-air_temp_A_forecast, air_temp_A_test_data, air_temp_A_mae, air_temp_A_mse, air_temp_A_r2, air_temp_A_exec_time, air_temp_A_model = linear_regression_projector(source_DF=source_dataframe, features=features,target="AirTemperatureA",developer_mode=developer_mode)
+if deploy_switch == 1:
+    # - Air temperature A -
+    air_temp_A_forecast, air_temp_A_test_data, air_temp_A_mae, air_temp_A_mse, air_temp_A_r2, air_temp_A_exec_time, air_temp_A_model = linear_regression_projector(source_DF=source_dataframe, features=features,target="AirTemperatureA",developer_mode=developer_mode)
 
-air_temp_A_metrics = pd.DataFrame({
-    "MAE": [air_temp_A_mae],
-    "MSE": [air_temp_A_mse],
-    "R squared": [air_temp_A_r2],
-    "Execution time": [air_temp_A_exec_time],
-    "Model": [air_temp_A_model]
-    })
-if developer_mode == 1:
-    print()
-    print(air_temp_A_metrics)
-    print()
+    air_temp_A_metrics = pd.DataFrame({
+        "MAE": [air_temp_A_mae],
+        "MSE": [air_temp_A_mse],
+        "R squared": [air_temp_A_r2],
+        "Execution time": [air_temp_A_exec_time],
+        "Model": [air_temp_A_model]
+        })
+    if developer_mode == 1:
+        print()
+        print(air_temp_A_metrics)
+        print()
 
-# - Air temperature B -
-air_temp_B_forecast, air_temp_B_test_data, air_temp_B_mae, air_temp_B_mse, air_temp_B_r2, air_temp_B_exec_time, air_temp_B_model = linear_regression_projector(source_DF=source_dataframe, features=features,target="AirTemperatureB", developer_mode=developer_mode)
+    # - Air temperature B -
+    air_temp_B_forecast, air_temp_B_test_data, air_temp_B_mae, air_temp_B_mse, air_temp_B_r2, air_temp_B_exec_time, air_temp_B_model = linear_regression_projector(source_DF=source_dataframe, features=features,target="AirTemperatureB", developer_mode=developer_mode)
 
-air_temp_B_metrics = pd.DataFrame({
-    "Column used:": "Air temperature B",
-    "MAE": [air_temp_B_mae],
-    "MSE": [air_temp_B_mse],
-    "R squared": [air_temp_B_r2],
-    "Execution time": [air_temp_B_exec_time],
-    "Model": [air_temp_A_model]
-    })
-if developer_mode == 1:
-    print()
-    print(air_temp_B_metrics)
-    print()
+    air_temp_B_metrics = pd.DataFrame({
+        "Column used:": "Air temperature B",
+        "MAE": [air_temp_B_mae],
+        "MSE": [air_temp_B_mse],
+        "R squared": [air_temp_B_r2],
+        "Execution time": [air_temp_B_exec_time],
+        "Model": [air_temp_A_model]
+        })
+    if developer_mode == 1:
+        print()
+        print(air_temp_B_metrics)
+        print()
 
-# - Air humidity -
-air_humidity_forecast, air_humidity_test_data, air_humidity_mae, air_humidity_mse, air_humidity_r2, air_humidity_exec_time, air_humidity_model = linear_regression_projector(source_DF=source_dataframe, features=features, target="AirHumidity", developer_mode=developer_mode)
+    # - Air humidity -
+    air_humidity_forecast, air_humidity_test_data, air_humidity_mae, air_humidity_mse, air_humidity_r2, air_humidity_exec_time, air_humidity_model = linear_regression_projector(source_DF=source_dataframe, features=features, target="AirHumidity", developer_mode=developer_mode)
 
-air_humidity_metrics = pd.DataFrame({
-    "Column used:": "Air humidity",
-    "MAE": [air_humidity_mae],
-    "MSE": [air_humidity_mse],
-    "R squared": [air_humidity_r2],
-    "Execution time": [air_humidity_exec_time],
-    "Model": [air_humidity_model]
-    })
-if developer_mode == 1:
-    print()
-    print(air_humidity_metrics)
-    print()
+    air_humidity_metrics = pd.DataFrame({
+        "Column used:": "Air humidity",
+        "MAE": [air_humidity_mae],
+        "MSE": [air_humidity_mse],
+        "R squared": [air_humidity_r2],
+        "Execution time": [air_humidity_exec_time],
+        "Model": [air_humidity_model]
+        })
+    if developer_mode == 1:
+        print()
+        print(air_humidity_metrics)
+        print()
 
 project_end = datetime.datetime.now() - project_start
 # ---> Document the processing time <---
